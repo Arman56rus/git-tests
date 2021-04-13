1a5f2aa625108385b766cb535dc05d2698260f2d
Victor Visockiy <victorvisockiy@noorsoft.ru>
2021-04-13 23:59:20 +0300

    Added input for random numbers generator max value

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const resultElem = document.querySelector('#result');
 const buttonElem = document.querySelector('#generate_button');
+const buttonElem = document.querySelector('#btn');
+const maxValueInputElem = document.querySelector('#max_value_input');
 
-const generateRandomNumber = () => Math.round(1000 * Math.random());
+const generateRandomNumber = (maxValue) => Math.round(maxValue * Math.random());
 
 buttonElem.addEventListener("click", () => {
-    const result = generateRandomNumber();
+    const result = generateRandomNumber(maxValueInputElem.value);
     resultElem.textContent = `Result: ${result}`;
 });
